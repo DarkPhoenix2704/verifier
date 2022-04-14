adf1b959f446fe7ca94d98e41a59e05b823dc667
DarkPhoenix2704 <anbarasun123@gmail.com>
2022-04-14 15:41:17 +0530

    Added Bottleneck

---
+++ src/connector.js
@@ -0,0 +1,7 @@
+const Bottleneck = require("bottleneck/es5");
+
+const limiter = new Bottleneck({
+	minTime: 333,
+});
+
+module.exports = { limiter };
--- src/events/guildMemberRemove.js
+++ src/events/guildMemberRemove.js
@@ -1,16 +1,21 @@
 const Airtable = require("airtable");
+const { limiter } = require("../connector");
 const base = new Airtable({ apiKey: process.env.AIRTABLE_API }).base("appyvgarul3TRSv43");
 
 module.exports = {
 	name: "guildMemberRemove",
 	async execute(member) {
-		base("Discord Users").select({
-			view: "All Discord Users",
-			filterByFormula: `ID = "${member.id}"`,
-		}).eachPage((records) => {
-			records.forEach((record) => {
-				base("Discord Users").destroy(record.id, (err) => console.log(err));
-			});
-		}, (err) => console.log(err));
+		limiter.schedule(() => {
+			base("Discord Users").select({
+				view: "All Discord Users",
+				filterByFormula: `ID = "${member.id}"`,
+			}).eachPage((records) => {
+				records.forEach((record) => {
+					limiter.schedule(() => {
+						base("Discord Users").destroy(record.id, (err) => console.log(err));
+					});
+				});
+			}, (err) => console.log(err));
+		});
 	},
 };
